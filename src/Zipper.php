9167eced9d21e41efde22f47fbb5c75bb53403c3
vladayson <vladaysin@gmail.com>
2020-07-22 12:08:20 +0300

    fix it

--- src/Zipper.php
+++ src/Zipper.php
@@ -1,11 +1,13 @@
 <?php
 
-
 namespace vladayson\zipper;
-
 
 use ZipArchive;
 
+/**
+ * Class Zipper
+ * @package vladayson\zipper
+ */
 class Zipper
 {
     public static function create($files, $destination, $password = null)
@@ -13,12 +15,14 @@ class Zipper
         $zip = new ZipArchive();
         $zip->open($destination, ZipArchive::CREATE);
         foreach ($files as $file) {
-            $zip->addFile($file);
+            $folder = dirname($file);
+            $basename = basename($file);
+            chdir($folder);
+            $zip->addFromString($basename, file_get_contents($file));
+            if ($password) {
+                $zip->setEncryptionName($basename, ZipArchive::EM_AES_256, $password);
+            }
         }
-        if ($password) {
-            $zip->setPassword($password);
-        }
-
         $zip->close();
     }
 }
